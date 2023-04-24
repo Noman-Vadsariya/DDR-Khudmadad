b5eb91fd3b08500047617e10e8198c107b4d75df
Noman-Vadsariya <noman.nooruddin02@gmail.com>
2023-04-24 15:01:13 +0500

    EfModels Finalized

--- DDR_Khudmadad/Program.cs
+++ DDR_Khudmadad/Program.cs
@@ -4,9 +4,9 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-//builder.Services.AddDbContext<Ef_DataContext>(
-//    o => o.UseSqlServer(builder.Configuration.GetConnectionString("AzureSqlDb"))
-//);
+builder.Services.AddDbContext<Ef_DataContext>(
+    o => o.UseSqlServer(builder.Configuration.GetConnectionString("AzureSqlDb"))
+);
 
 //Automatically perform migration
 //builder.Services.BuildServiceProvider().GetService<Ef_DataContext>().Database.Migrate();
